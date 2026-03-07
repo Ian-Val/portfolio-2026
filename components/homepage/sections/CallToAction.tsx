d018c288c7555dac0b8c95d8626611ee923d2909
Ian Valeta <valeta.iac@gmail.com>
2026-03-07 15:11:52 -0500

    updated cta

--- components/homepage/sections/CallToAction.tsx
+++ components/homepage/sections/CallToAction.tsx
@@ -15,16 +15,14 @@ export default function CallToAction() {
       >
         <div className="sm:w-auto sm:p-0 w-full p-10">
           <div className="bg-white rounded-lg p-8 border-2 border-blue-600 w-full">
-            <h3 className="text-center mb-2">
+            <h2 className="text-center text-2xl mb-2">
               Request a{" "}
               <span className="inline-block bg-amber-400 text-neutral-900 px-2 py-1 rounded">
                 Free
               </span>{" "}
               Consultation
-            </h3>
-            <small className="block text-center mb-2">
-              Let's Build Something Amazing Together
-            </small>
+            </h2>
+
             <form className="flex flex-col gap-1">
               <label htmlFor="name">Name</label>
               <input
@@ -54,31 +52,29 @@ export default function CallToAction() {
                 Tell us more about your project
               </label>
               <textarea
-                className="rounded bg-blue-100 border border-blue-600 p-2"
+                className="rounded bg-blue-100 border border-blue-600 p-2 min-h-20"
                 name="description"
                 id="description"
                 cols={10}
                 rows={4}
                 placeholder="What are you looking for?"
               ></textarea>
-              <button className="mt-4 bg-amber-400 text-neutral-900 px-2 py-1 rounded">
+              <button className="mt-4 bg-blue-600 text-white px-2 py-2 rounded">
                 Next
               </button>
             </form>
           </div>
         </div>
         <div className="bg-amber-400 sm:p-8 p-10 w-full flex flex-col justify-center sm:rounded-lg my-auto">
-          <h2 className="mb-4 text-2xl sm:text-start text-center">
-            <span className="sm:inline hidden">←</span> Transform Your Digital
-            Presence
+          <h2 className="mb-4 text-xl sm:text-start text-center">
+            <span className="sm:inline hidden">←</span> Let's Build Something
+            Awesome
           </h2>
-          <small className="block mb-4 text-blue-600 sm:text-start text-center">
-            In 3 Steps
-          </small>
+
           <ol className="list-decimal list-inside flex flex-col gap-3 sm:mx-0 mx-auto">
-            <li>Fill out the request form.</li>
-            <li>Pick a time to meet.</li>
-            <li>Let's discuss your web goals.</li>
+            <li>Fill out the form.</li>
+            <li>Choose a time to meet.</li>
+            <li>Discuss your website goals.</li>
           </ol>
           <span className="sm:hidden block text-center mt-6">↓</span>
         </div>
