83e2f3cb69f69112cd684cbfa3ba3bff5952928b
Ian Valeta <valeta.iac@gmail.com>
2026-03-15 13:48:49 -0400

    Update Hero.tsx

--- components/homepage/sections/Hero.tsx
+++ components/homepage/sections/Hero.tsx
@@ -14,21 +14,17 @@ export default function Hero() {
             Ian Valeta
           </h1>
           <p className="my-6 max-w-90 md:text-start text-center md:ml-0 mx-auto font-normal">
-            I am a recent college graduate looking to land my first full-time
-            role in software development.
+            I am a college graduate looking to land my first full-time role in
+            software development.
           </p>
-          <ul className="mt-8 mb-8 flex gap-2 md:max-w-90 md:justify-start justify-center flex-wrap ">
-            <li className="chip">HTML</li>
-            <li className="chip">CSS</li>
-            <li className="chip">TypeScript</li>
-            <li className="chip">PostgreSQL</li>
-            <li className="chip">MongoDB</li>
-            <li className="chip">Express.js</li>
-            <li className="chip">React.js</li>
-            <li className="chip">Node.js</li>
-            <li className="chip">Next.js</li>
-            <li className="chip">Electron.js</li>
-          </ul>
+          <div className="flex gap-4">
+            <Link
+              href="https://www.linkedin.com/in/ianvaleta/"
+              className="button-primary"
+            >
+              Let's Connect on LinkedIn
+            </Link>
+          </div>
         </div>
         <div className="w-full">
           <Image
