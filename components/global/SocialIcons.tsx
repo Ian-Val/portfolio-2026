1b367bc8549359ad8536961abb2e0ec5de48a6b7
Ian Valeta <valeta.iac@gmail.com>
2026-04-08 11:36:11 -0400

    fixed errors

--- components/global/SocialIcons.tsx
+++ components/global/SocialIcons.tsx
@@ -4,15 +4,15 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialIcons() {
   return (
-    <div className="flex gap-4 md:mx-0 mx-auto mb-4 md:items-start items-center">
+    <div className="flex gap-4 mb-8 md:justify-start justify-center">
       <a
-        className="button-primary p-1! text-4xl"
+        className="block button-primary p-1! text-4xl"
         href="https://linkedin.com/in/ianvaleta"
       >
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
       <a
-        className="button-primary p-1! text-4xl"
+        className="block button-primary p-1! text-4xl"
         href="https://github.com/Ian-Val"
       >
         <FontAwesomeIcon icon={faGithub} />
