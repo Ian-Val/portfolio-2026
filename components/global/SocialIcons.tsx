5b83060efbf9ace8321c777193b123a1ea8e5a83
Ian Valeta <valeta.iac@gmail.com>
2026-04-08 09:37:23 -0400

    Update SocialIcons.tsx

--- components/global/SocialIcons.tsx
+++ components/global/SocialIcons.tsx
@@ -4,7 +4,7 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialIcons() {
   return (
-    <div className="flex gap-4 md:mx-0 mx-auto mb-4">
+    <div className="flex gap-4 md:mx-0 mx-auto mb-4 md:items-start items-center">
       <a
         className="button-primary p-1! text-4xl"
         href="https://linkedin.com/in/ianvaleta"
