df6b85bf99112fc2bb099221ab65a9f708764bf2
Ian Valeta <valeta.iac@gmail.com>
2026-05-10 23:11:43 -0400

    added snippet libary

--- components/homepage/sections/Work.tsx
+++ components/homepage/sections/Work.tsx
@@ -10,7 +10,57 @@ export default function Work() {
         <span className="bg-white text-blue-900 px-2 py-1 rounded">Work</span>
       </h2>
       <p className="my-4 text-center">↓</p>
-      <div className="flex">
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col items-center w-full gap-4 px-10 py-10 border-2 rounded-2xl border-blue-600 bg-white text-neutral-900 ">
+          <Link
+            href="https://html-snippet-library.vercel.app/"
+            className=" text-neutral-900"
+          >
+            <h2 className="text-3xl">Snippet Library</h2>
+          </Link>
+          <div className="text-start w-full flex flex-col gap-5">
+            <p>
+              The{" "}
+              <Link
+                className="underline text-blue-900"
+                href="https://html-snippet-library.vercel.app/"
+              >
+                Snippet Library
+              </Link>{" "}
+              is a client-side React application that eliminates repetitive
+              drag-and-drop work in marketing workflows by providing a
+              centralized repository of pre-built, tested HTML/CSS components.
+              Its core purpose is to accelerate campaign and landing page
+              development, allowing marketers to assemble professional pages in
+              minutes rather than hours by reusing proven components instead of
+              rebuilding from scratch each time.
+            </p>
+
+            <p>Key Features:</p>
+
+            <ul className="list-disc list-inside flex flex-col gap-5">
+              <li>
+                Reusable Component Library: Store pre-built HTML/CSS snippets
+              </li>
+
+              <li>Live Preview: See changes in real-time before saving</li>
+
+              <li>
+                Isolated CSS Scoping: Prevents style conflicts between
+                components
+              </li>
+
+              <li>
+                Responsive Testing: Toggle desktop, tablet, and mobile views
+              </li>
+
+              <li>
+                One-Click Copy: Export full HTML+CSS blocks with unique IDs for
+                each instance
+              </li>
+            </ul>
+          </div>
+        </div>
         <div className="flex flex-col items-center w-full md:min-h-screen gap-4 px-10 py-10 border-2 rounded-2xl border-blue-600 bg-white text-neutral-900 ">
           <Link href="https://www.abecopark.com" className=" text-neutral-900">
             <h2 className="text-3xl">AB EcoPark</h2>
